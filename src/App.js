71d722229fbd4318f597ac196b5d3080ae0f18d4
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-07-09 23:54:53 +0530

    improving the Date counter app with range input type and reset button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
+
 export default function App() {
   return (
     <div className="App">
@@ -20,17 +20,33 @@ function Counter() {
   function handleNext() {
     setCount((c) => count + step);
   }
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
   return (
     <div>
       <h1>Date-Counter-App</h1>
       <div>
-        <button onClick={() => setStep((e) => e - 1)}>-</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {/* <button onClick={() => setStep((e) => e - 1)}>-</button> */}
         <span>Step:{Math.abs(step)}</span>
-        <button onClick={() => setStep((e) => e + 1)}>+</button>
+        {/* <button onClick={() => setStep((e) => e + 1)}>+</button> */}
       </div>
       <div>
         <button onClick={handlePrevious}>-</button>
-        <span>Count:{count}</span>
+        {/* <span>Count:{count}</span> */}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={handleNext}>+</button>
       </div>
       <p>
@@ -43,6 +59,9 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      <div>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
